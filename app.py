260e30dc29688a253119bf9f94b09a5e527016bd
pshmaharana-code <pshmaharana@gmail.com>
2025-10-12 00:18:49 +0530

    Milestone-HMS Auth-RBAC

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -11,6 +11,17 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 #Initialize extention
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # Redirect to login page if the user is not logged in.
+
+@login_manager.user_loader #The user_loader function is required by Flask-Login. 
+#It's used to retrieve a user from the database based on the ID that Flask-Login stores in the session.
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 
 
@@ -30,14 +41,27 @@ def login():
 
         #check if user exist and password is correct
         if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user) #Logs the user in
             #for now we'll just show a massage
             #later we'll redirect it to correct dashboard.
             flash('Login Successful!', 'success')
-            return redirect (url_for('home')) #redirect to home on success
+            
+            if user.role == "admin":
+                return redirect(url_for('admin_dashboard'))
+            elif user.role == "doctor":
+                return redirect(url_for('doctor_dashboard'))
+            elif user.role == "patient":
+                return redirect(url_for('patient_dashboard'))
         else:
             flash('Invalid username or password, Please try again.', 'danger')
 
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user() #Logs the user out
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -72,6 +96,30 @@ def register():
         return redirect(url_for('login'))
     
     return render_template('register.html')
+
+
+@app.route('/admin/dashboard')
+@login_required
+def admin_dashboard():
+    #Authorization check : only admin can access this page
+    if current_user.role != 'admin':
+        abort(403)
+    return render_template('admin_dashboard.html')
+
+@app.route('/doctor/dashboard')
+@login_required
+def doctor_dashboard():
+    if current_user.role != 'doctor':
+        abort(403)
+    return render_template('doctor_dashboard.html')
+
+@app.route('/patient/dashboard')
+@login_required
+def patient_dashboard():
+    if current_user.role != 'patient':
+        abort(403)
+    return render_template('patient_dashboard.html')
+
 
 
 
